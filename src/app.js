abae716cedd398e2ec0d586422f33c018054e3ba
sum9191-ops <sum9191@gmail.com>
2026-06-12 18:44:56 +0900

    Revert ioredis TLS DNS resolution fixes

--- src/app.js
+++ src/app.js
@@ -44,10 +44,7 @@ const redis = isLocalRedis
   ? new Redis({ host: config.redis.host, port: config.redis.port })
   : new Redis.Cluster(
     [{ host: config.redis.host, port: config.redis.port }],
-    { 
-      redisOptions: { tls: { rejectUnauthorized: false } },
-      dnsLookup: (address, callback) => callback(null, address)
-    }
+    { redisOptions: { tls: {} } }
   );
 
 redis.on('connect', () => console.log('⚡ Redis 캐시 서버 연결 완료!'));
